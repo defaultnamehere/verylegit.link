59c3d8cdac420ea8dc66d1092b43b3b5bbd3cd74
Alex 'gags' Hogue <ahogue@atlassian.com>
2016-09-07 10:12:34 +1000

    Change database to firebased

--- database.py
+++ database.py
@@ -1,56 +1,54 @@
 
 import datetime
+import json
 
-# Literally WiredTiger
-# I once found a bug in WiredTiger in which you couldn't store 32-bit ints. You just couldn't do it.
-import pymongo
+import pyrebase
 
+
+FIREBASE_CONFIG_PATH = "firebase_config.json"
 
 class URLStoreModel():
 
     def __init__(self):
-        # Set up connection to the database.
-        self.client = pymongo.MongoClient('localhost', 27017)
-        self.url_database = self.client.urls
-        self.url_collection = self.url_database.urls
+
+        with open(FIREBASE_CONFIG_PATH) as f:
+            self.config = json.load(f)
+
+        firebase = pyrebase.initialize_app(self.config)
+
+        self.db = firebase.database()
 
     def set_url(self, long_url, sketchy_url):
 
         # Only care about the part after the slash, since we don't care about the domain
-        document = {
-            "long_url" : long_url,
+        data = {
+            "long_url": long_url,
             "sketchy_url": sketchy_url,
             "clicks": 0,
             "created": datetime.datetime.utcnow()
         }
 
-        self.url_collection.insert_one(document)
+        # Set up a mapping both ways.
+        self.db.child("urls").child(long_url).set(data)
+        self.db.child("urls").child(sketchy_url).set(data)
 
     def get_long_url(self, sketchy_url):
 
-        url_document = self.url_collection.find_one({
-            "sketchy_url": sketchy_url
-        })
+        url_data = self.db.child("urls").child(sketchy_url).get().val()
 
         # None if we don't have this URL already
-        if url_document is None:
+        if not url_data:
             return None
 
-        url_id = url_document["_id"]
 
-        # Increment the clicks
-        self.url_collection.update_one({'_id': url_id}, {'$inc': {'clicks': 1}})
-
-        return url_document["long_url"]
+        return url_data["long_url"]
 
     def get_sketchy_url(self, long_url):
 
-        url_document = self.url_collection.find_one({
-            "long_url": long_url 
-        })
+        url_data = self.db.child("urls").child(long_url).get().val()
 
         # None if we don't have this URL already
-        if url_document is None:
+        if not url_data:
             return None
 
-        return url_document["sketchy_url"]
+        return url_data["sketchy_url"]
